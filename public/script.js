654379026042819293a9897653ca82e0b5e15cfd
Druum Dev <Tootdev2@gmail.com>
2026-04-01 00:23:23 -0300

    script.js

--- public/script.js
+++ public/script.js
@@ -2,42 +2,9 @@ let isAdmin = localStorage.getItem("admin") === "true";
 
 // 👑 LOGIN / ENVIO
 function enviar() {
-    const nick = document.getElementById("nick").value.trim();
-    const id = document.getElementById("id").value.trim();
-
-    BARREIRA ANTI-FUNCIONAMENTO KKKKKKKK
-
-    // LOGIN ADMIN
-    if (nick === "C#Lipeh777" && id === "1234") {
-        isAdmin = true;
-        localStorage.setItem("admin", "true");
-        alert("👑 Admin logado!");
-        carregar();
-        return;
-    }
-
-    // validação
-    if (!nick || !id) {
-        alert("Preencha os dois campos!");
-        return;
-    }
-
-    fetch("https://copabrawl.onrender.com/add", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ nick, id })
-    })
-    .then(res => res.text())
-    .then(data => {
-        if (data !== "ok") alert("Erro: " + data);
-        carregar();
-    })
-    .catch(err => {
-        console.error("Erro:", err);
-        alert("Erro ao enviar");
-    });
+    alert("Envios estão desativados!");
+    return;
 }
-
 // 📋 CARREGAR LISTA
 function carregar() {
     fetch("https://copabrawl.onrender.com/list")
